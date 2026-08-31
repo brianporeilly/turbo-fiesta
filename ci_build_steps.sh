a64dc4a08dfb6219723f74e69363782c1f457b17
Brian O'Reilly <brian@oreillys.io>
2026-08-31 09:25:06 -0800

    Update ci_build_steps.sh

--- ci_build_steps.sh
+++ ci_build_steps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+sed -i 's/^FLATCAR_BUILD_ID=.*/FLATCAR_BUILD_ID="local"/' /mnt/host/source/.repo/manifests/version.txt
+
 rm -f /build/amd64-usr/var/tmp/portage/sys-kernel/coreos-modules-*/.compiled 2>/dev/null || true
 emerge-amd64-usr sys-kernel/coreos-modules
 emerge-amd64-usr sys-kernel/coreos-kernel
